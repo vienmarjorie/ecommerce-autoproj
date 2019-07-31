5b6f501c8cccd77b38f28aee82628a5366bae7ff
Viennah.marilao <Viennah.marilao@LPP000121042.fsoft.fpt.vn>
2019-07-31 14:26:06 +0800

    CommonFunctionality: created implicitWait() method

--- AutomationProject/src/common/CommonFunctionality.java
+++ AutomationProject/src/common/CommonFunctionality.java
@@ -2,11 +2,20 @@ package common;
 
 import common.Browsers;
 import common.Variables;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 
 public class CommonFunctionality {
 	public static WebDriver driver = null;
 	public static WebElement element = null;
+	
+	// Registration
+	public void registration(){
+		Browsers.driver.findElement(By.xpath("//*[contains(@title, 'Log in')]")).click();
+		Browsers.driver.findElement(By.xpath("//*[@name='email_create']")).click();
+	}
 	
 	//Sign-in
 	public void login() {
@@ -16,6 +25,10 @@ public class CommonFunctionality {
 		Browsers.driver.findElement(By.xpath("//*[contains(@type, 'pass')]")).click();
 		Browsers.driver.findElement(By.xpath("//*[contains(@type, 'pass')]")).sendKeys(Variables.registeredPassword);
 		Browsers.driver.findElement(By.xpath("//*[@id='SubmitLogin']")).click();
+	}
+	
+	public void implicitWait(){
+		Browsers.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 }
 
