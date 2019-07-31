83d066047ed3e3bf38cee001508496e2f5ae36de
vienmarjorie <vienmarilao@gmail.com>
2019-07-31 22:30:37 +0800

    Variables: added variable for unregisteredEmail

--- AutomationProject/src/common/Variables.java
+++ AutomationProject/src/common/Variables.java
@@ -6,5 +6,6 @@ public class Variables {
 	public static final String newEmailAccount = "test.slnx2@gmail.com";
 	public static final String registeredEmail = "test.slnx1@gmail.com";
 	public static final String registeredPassword = "Aptraxfs1";
+	public static final String unregisteredEmail = "test.slnx10@gmail.com";
 	 
 }
