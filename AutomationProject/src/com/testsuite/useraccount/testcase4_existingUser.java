d78c5d0837338938a27f8a39ddad1fdb94dabf59
Viennah.marilao <Viennah.marilao@LPP000121042.fsoft.fpt.vn>
2019-07-31 14:05:35 +0800

    commit - testcase4_existingUser

---
+++ AutomationProject/src/com/testsuite/useraccount/testcase4_existingUser.java
@@ -0,0 +1,37 @@
+package com.testsuite.useraccount;
+
+import common.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class testcase4_existingUser {
+	
+	static Browsers browser = new Browsers();
+	static CommonFunctionality common = new CommonFunctionality();
+	
+	@BeforeTest
+	public static void openBrowser(){
+		browser.ChromeBrowser();
+	}
+	
+	@AfterTest
+	public static void closeBrowser(){
+		browser.closeBrowser();
+	}
+	
+   @Test
+   public void existingUser() {
+	   common.registration();
+	   Browsers.driver.findElement(By.xpath("//*[@name='email_create']")).sendKeys(Variables.registeredEmail);
+	   Browsers.driver.findElement(By.xpath("//*[@id='SubmitCreate']")).click();
+	   Browsers.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	   String expected = "An account using this email address has already been registered. Please enter a valid password or request a new one.";
+	   String actual = Browsers.driver.findElement(By.xpath("//*[@id='create_account_error']/ol/li")).getText();
+	   System.out.println(actual);
+	   Assert.assertEquals(actual, expected);
+   }
+}
