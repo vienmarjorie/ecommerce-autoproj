c4fdb18733f915f58943d582176634483013127a
vienmarjorie <vienmarilao@gmail.com>
2019-08-02 00:44:59 +0800

    Variables: added variables

--- AutomationProject/src/common/Variables.java
+++ AutomationProject/src/common/Variables.java
@@ -3,9 +3,14 @@ package common;
 public class Variables {
 
 	 //Text fields
-	public static final String newEmailAccount = "test.slnx2@gmail.com";
+	public static final String emailRegistration = "test.slnx2@gmail.com";
+	public static final String emailRegPassword = "Adminpw123";
+	public static final String invalidRegPassword = "Adm";
 	public static final String registeredEmail = "test.slnx1@gmail.com";
 	public static final String registeredPassword = "Aptraxfs1";
 	public static final String unregisteredEmail = "test.slnx10@gmail.com";
+	public static final String firstName = "Jean";
+	public static final String lastName = "Sparrow";
 	 
+	public static final int minPassword = 5;
 }
