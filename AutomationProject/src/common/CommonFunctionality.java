8b3650984a2703a625155458863d268b9673d699
vienmarjorie <vienmarilao@gmail.com>
2019-08-03 01:45:08 +0800

    updated code

--- AutomationProject/src/common/CommonFunctionality.java
+++ AutomationProject/src/common/CommonFunctionality.java
@@ -6,20 +6,27 @@ import common.Variables;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonFunctionality {
 	public static WebDriver driver = null;
 	public static WebElement element = null;
+	int timeOut = 20;
 	
 	// Registration
 	public void registration(){
-		Browsers.driver.findElement(By.xpath("//*[contains(@title, 'Log in')]")).click();
+		String text = "Sign in";
+		new WebDriverWait (Browsers.driver, timeOut).until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"), text));
+		Browsers.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
 		Browsers.driver.findElement(By.xpath("//*[@name='email_create']")).click();
 	}
 	
 	//Sign-in
 	public void login() {
-		Browsers.driver.findElement(By.xpath("//*[contains(@title, 'Log in')]")).click();
+		String text = "Sign in";
+		new WebDriverWait (Browsers.driver, timeOut).until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"), text));
+		Browsers.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
 		Browsers.driver.findElement(By.xpath("//input[@name='email']")).click();
 		Browsers.driver.findElement(By.xpath("//input[@name='email']")).sendKeys(Variables.registeredEmail);
 		Browsers.driver.findElement(By.xpath("//*[contains(@type, 'pass')]")).click();
@@ -27,6 +34,11 @@ public class CommonFunctionality {
 		Browsers.driver.findElement(By.xpath("//*[@id='SubmitLogin']")).click();
 	}
 	
+	public void logout() {
+		String text = "Sign out";
+		new WebDriverWait (Browsers.driver, timeOut).until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[2]/a"), text));
+		Browsers.driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[2]/a")).click();
+	}
 	public void implicitWait(){
 		Browsers.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
--- AutomationProject/src/common/Variables.java
+++ AutomationProject/src/common/Variables.java
@@ -3,7 +3,7 @@ package common;
 public class Variables {
 
 	 //Text fields
-	public static final String emailRegistration = "test.slnx2@gmail.com";
+	public static final String emailRegistration = "test.slnx5@gmail.com";
 	public static final String emailRegPassword = "Adminpw123";
 	public static final String invalidRegPassword = "Adm";
 	public static final String registeredEmail = "test.slnx1@gmail.com";
@@ -11,6 +11,14 @@ public class Variables {
 	public static final String unregisteredEmail = "test.slnx10@gmail.com";
 	public static final String firstName = "Jean";
 	public static final String lastName = "Sparrow";
-	 
+	public static final String company = "Alphabet Corporation";
+	public static final String address = "1600 Ampitheatre Parkway Mountain View";
+	public static final String city = "California";
+	public static final String postCode = "90001";
+	public static final String phoneNumber = "+639234567890";
+	public static final String aliasEmail = "test.slnx10@gmail.com";
+	public static final String passwordErrorMsg = "passwd is invalid.";
+		 
 	public static final int minPassword = 5;
+	
 }
